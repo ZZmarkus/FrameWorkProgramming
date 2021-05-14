6c3d5fa695d2dcc45c720cd2a9faac4d9ea808fc
HMC <1170313@naver.com>
2021-05-14 12:22:53 +0900

    remove file

--- 7장 스프링 AOP/h_aop_xml/src/main/java/org/kpu/di/persistence/MemberDAO.java
+++
@@ -1,8 +0,0 @@
-package org.kpu.di.persistence;
-
-import org.kpu.di.domain.StudentVO;
-
-public interface MemberDAO {
-	public void add(StudentVO student) throws Exception;
-	public StudentVO read(String id) throws Exception;
-}
